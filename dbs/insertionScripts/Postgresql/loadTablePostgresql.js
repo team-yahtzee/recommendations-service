1fa1dc77a17808e5c41a0ff2a7288a28072cab53
bengold14 <bengold14@gmail.com>
2019-06-08 11:59:20 -0400

    added env variables seed and total to postgresql

--- dbs/insertionScripts/Postgresql/loadTablePostgresql.js
+++ dbs/insertionScripts/Postgresql/loadTablePostgresql.js
@@ -18,25 +18,6 @@ const client = new Client({
 })
 client.connect()
 
-// id i
-// |
-// user faker.userName
-// |
-// recImg varchar(255)
-// |
-// recDetails varchar(255)
-// |
-// recTitle varchar(255)
-// |
-// recCost varchar(255)
-// |
-// recRating varchar(255)
-// |
-// recratingCount varchar(255)
-// |
-// roomId integer(50)
-// recImg,recDetails,recTitle,recCost,recRating,recratingCount,roomId
-
 let t0 = performance.now()
 let targetTable = 'rooms'
 
@@ -68,34 +49,12 @@ client.query(`CREATE TABLE ${targetTable}(
   console.log('created table!')
 })
 
-// let i = 0
-
-// client.query(`PREPARE roomInsertion (integer, varchar (100), varchar(255), varchar(255), varchar(255), varchar(255), varchar(255), varchar(255)) AS
-//     INSERT INTO rooms VALUES($1, $2, $3, $4, $5, $6, $7, $8);`)
-// .catch(err=>{
-//   console.log('error preparing insertion', err)
-// })
-
-// while ( i < 1000) {
-//   let values = `(` + JSON.stringify(i) + ',' + 
-//   "'" + faker.internet.userName() + "'" + ',' + 
-//   "'" + faker.image.imageUrl() + "'" + ',' + 
-//   "'" + faker.lorem.sentence() + "'" + ',' + 
-//   "'" + faker.lorem.words() + "'" + ',' + 
-//   "'" + faker.finance.amount() + "'" + ',' + 
-//   "'" + faker.random.number() + "'" + ',' + 
-//   "'" + faker.random.number() + "'" + `)`
-//   client.query(`EXECUTE roomInsertion ${values};`)
-//   // client.query(`insert INTO rooms (id, username) VALUES (${JSON.stringify(i)}, 'ben')`)
-//   .catch((error) => {
-//     console.log(`Error in copy command: ${error}`)
-//   })
-//   i++
-// }
+let seed = +process.env.seed
+let total = +process.env.total
 
 const insertQueries = (count = 0) => {
-  let i = count * 50000
-  let end = (count * 50000) + 50000
+  let i = count * seed
+  let end = (count * seed) + seed
   let values = ''
   while ( i < end) {
     let value = `(` + JSON.stringify(i) + ',' + 
@@ -122,7 +81,7 @@ const insertQueries = (count = 0) => {
     VALUES ${values}`
   )
   .then(()=>{
-    if (count < 199) {
+    if (count < ((total/seed) - 1)) {
       count ++
       insertQueries(count)
     } else {
@@ -137,59 +96,3 @@ const insertQueries = (count = 0) => {
 
 insertQueries()
 
-// Execute Copy Function
-// var stream = client.query(copyFrom(`COPY ${targetTable} FROM CSV HEADER STDIN`))
-// var fileStream = fs.createReadStream(inputFile)
-
-// fileStream.on('error', (error) =>{
-//   console.log(`Error in reading file: ${error}`)
-// })
-// stream.on('error', (error) => {
-//   console.log(`Error in copy command: ${error}`)
-// })
-// stream.on('end', () => {
-//   console.log(`Completed loading data into ${targetTable}`)
-//   client.end()
-// })
-// fileStream.pipe(stream);
-
-// // Connecting to Database
-// const client = new Client({
-//   connectionString: conString,
-// })
-
-// client.connect()
-
-// const executeQuery = (targetTable) => {
-//   const execute = (target, callback) => {
-//       client.query(`Truncate ${target}`, (err) => {
-//               if (err) {
-//               client.end()
-//               callback(err)
-//               // return console.log(err.stack)
-//               } else {
-//               console.log(`Truncated ${target}`)
-//               callback(null, target)
-//               }
-//           })
-//   }
-//   execute(targetTable, (err) =>{
-//       if (err) return console.log(`Error in Truncate Table: ${err}`)
-//       var stream = client.query(copyFrom(`COPY ${targetTable} FROM STDIN`))
-//       var fileStream = fs.createReadStream(inputFile)
-      
-//       fileStream.on('error', (error) =>{
-//           console.log(`Error in creating read stream ${error}`)
-//       })
-//       stream.on('error', (error) => {
-//           console.log(`Error in creating stream ${error}`)
-//       })
-//       stream.on('end', () => {
-//           console.log(`Completed loading data into ${targetTable}`)
-//           client.end()
-//       })
-//       fileStream.pipe(stream);
-//   })  
-// }
-// // Execute the function
-// executeQuery(table)
