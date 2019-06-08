e1f3f6409f29871d25d664435e97c0507f04f74b
bengold14 <bengold14@gmail.com>
2019-06-08 14:22:09 -0400

    added logging every 100,000 entries

--- dbs/insertionScripts/Mongo/loadTableMongo.js
+++ dbs/insertionScripts/Mongo/loadTableMongo.js
@@ -22,6 +22,9 @@ let total = +process.env.total
 
 const insertQueries = (count = 0) => {
   let i = count * seed
+  if (Number.isInteger(i/100000)) {
+    console.log('total seed = ',i)
+  }
   let end = (count * seed) + seed
   let values = []
   while (i < end) {
