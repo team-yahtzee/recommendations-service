518cc254ccb1437baccefb71fe28ddac5c4fd8e5
bengold14 <bengold14@gmail.com>
2019-06-20 10:32:02 -0400

    added redis functionality

--- dbs/insertionScripts/Mongo/mongoDB.js
+++ dbs/insertionScripts/Mongo/mongoDB.js
@@ -1,19 +1,23 @@
-const Mongoose = require('mongoose')
+const Mongoose = require("mongoose");
 
-const mongoPath = process.env.mongoPath
+const mongoPath = process.env.mongoPath;
 
-Mongoose.connect(`mongodb://3.130.244.88:27017/rooms`,{ useNewUrlParser: true })
+Mongoose.connect(`mongodb://3.130.246.122:27017/rooms`, {
+  useNewUrlParser: true
+});
 
-const Room = Mongoose.model('rooms',new Mongoose.Schema ({
-  id: Number,
-  username: String,
-  recImg: String,
-  recDetails: String,
-  recTitle: String,
-  recCost: Number,
-  recRating: Number,
-  recratingCount: Number
-}))
+const Room = Mongoose.model(
+  "rooms",
+  new Mongoose.Schema({
+    id: Number,
+    username: String,
+    recImg: String,
+    recDetails: String,
+    recTitle: String,
+    recCost: Number,
+    recRating: Number,
+    recratingCount: Number
+  })
+);
 
-module.exports.Room = Room
-
+module.exports.Room = Room;
